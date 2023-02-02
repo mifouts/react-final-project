ee95ab1465352c0540801cf104cd42db68e40d60
mifouts <fouts.mitch15@gmail.com>
2023-02-02 15:19:16 -0500

    editing landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,7 +2,41 @@ import React from "react";
 import "./Landing.css";
 
 function Landing() {
-  return <div></div>;
+  return (
+    <div>
+      <div class="container landing__container">
+        <h1 class="movieflix__title click">
+          <span class="red">MOVIEFLIX</span>
+        </h1>
+        <h3 class="movieflix__description click">
+          Find the perfect movie for you!
+        </h3>
+        <form
+          class="movieflix__search"
+          onchange="loadingMovies(event)"
+          onsubmit="searchBarForm(event)"
+        >
+          <input
+            type="text"
+            class="movieflix__input"
+            placeholder="Movie Name"
+            onchange="searchBarForm(event)"
+            autofocus
+          />
+          <button
+            class="submit__button nav__click"
+            type="submit"
+            onclick="searchBarForm(event)"
+          >
+            <i class="fa-solid fa-magnifying-glass search"></i>
+          </button>
+        </form>
+        <div class="movie__overlay movie__overlay--loading">
+          <i class="fas fa-spinner"></i>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Landing;
