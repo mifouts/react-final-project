19bf1f4e4c39f35184cfd565f43731813b7a13a8
mifouts <fouts.mitch15@gmail.com>
2023-02-03 10:01:27 -0500

    making background image look better

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -20,34 +20,34 @@ function Landing() {
 
   return (
     <div>
-      <div className="container landing__container">
-        <Link to={"/"}>
+      <body className="img__placeholder">
+        <div className="container landing__container">
           <h1 className="movieflix__title click">
             <span className="red">MOVIEFLIX</span>
           </h1>
-        </Link>
-        <h3 className="movieflix__description click">
-          Find the perfect movie for you!
-        </h3>
-        <form className="movieflix__search" /*onChange={""} onSubmit={""}*/>
-          <input
-            type="text"
-            className="movieflix__input"
-            placeholder="Movie Name"
-            /*onChange={""}*/
-          />
-          <button
-            className="submit__button nav__click"
-            type="submit"
-            /*onClick={""}*/
-          >
-            <i className="fa-solid fa-magnifying-glass search"></i>
-          </button>
-        </form>
-        <div className="movie__overlay movie__overlay--loading">
-          <i className="fas fa-spinner"></i>
+          <h3 className="movieflix__description click">
+            Find the perfect movie for you!
+          </h3>
+          <form className="movieflix__search" /*onChange={""} onSubmit={""}*/>
+            <input
+              type="text"
+              className="movieflix__input"
+              placeholder="Movie Name"
+              /*onChange={""}*/
+            />
+            <button
+              className="submit__button nav__click"
+              type="submit"
+              /*onClick={""}*/
+            >
+              <i className="fa-solid fa-magnifying-glass search"></i>
+            </button>
+          </form>
+          <div className="movie__overlay movie__overlay--loading">
+            <i className="fas fa-spinner"></i>
+          </div>
         </div>
-      </div>
+      </body>
     </div>
   );
 }
