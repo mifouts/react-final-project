95ad1134723d8a95387cc44914aca07e11e673e8
mifouts <fouts.mitch15@gmail.com>
2023-02-03 10:18:47 -0500

    fixing background image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
 import Landing from "./pages/Landing";
 import Nav from "./pages/Nav";
+import background from "./assets/backgroundimg.png";
 
 function App() {
+  const myStyle = {
+    backgroundImage: `url(${background})`,
+    backgroundPosition: "center",
+    height: "100vh",
+    width: "100vw",
+    marginTop: "0px",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  };
   return (
-    <div className="App">
-      <body className="img__placeholder">
-        <Nav />
-        <Landing />
-      </body>
+    <div className="App" style={myStyle}>
+      <Nav />
+      <Landing />
     </div>
   );
 }
