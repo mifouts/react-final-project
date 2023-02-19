f76f520b28866f6bda7e33d9340bd1a71b9be4dd
mifouts <fouts.mitch15@gmail.com>
2023-02-19 12:52:48 -0500

    movie styling

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -15,6 +15,17 @@ function Movie({ movie }) {
           </div>
         </div>
       </div>
+      <div className="movie__container click">
+        <div className="movie">
+          <div className="movie__poster">
+            <img src={movie.Poster} className="movie__poster--img" alt="" />
+          </div>
+          <div className="movie__info--container">
+            <div className="movie__title">{movie.Title}</div>
+            <div className="movie__year">{movie.Year}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
