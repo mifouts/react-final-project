6de5e0e98f2e6fd2173d1237effec8a8c2a0e3e8
mifouts <fouts.mitch15@gmail.com>
2023-02-19 13:00:37 -0500

    trying to style movies

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -3,96 +3,15 @@ import "./Movie.css";
 
 function Movie({ movie }) {
   return (
-    <div class="movies__container">
-      <div class="movie__container">
-        <div class="movie">
-          <div class="movie__poster">
-            <img
-              src="https://m.media-amazon.com/images/M/MV5BMTM3NTg2NDQzOF5BMl5BanBnXkFtZTcwNjc2NzQzOQ@@._V1_SX300.jpg"
-              class="movie__poster--img"
-              alt=""
-            ></img>
+    <div className="movies__container">
+      <div className="movie__container click">
+        <div className="movie">
+          <div className="movie__poster">
+            <img src={movie.Poster} className="movie__poster--img" alt="" />
           </div>
-          <div class="movie__info--container">
-            <div class="movie__title">Fast & Furious 6</div>
-            <div class="movie__year">2013</div>
-          </div>
-        </div>
-      </div>
-      <div class="movie__container">
-        <div class="movie">
-          <div class="movie__poster">
-            <img
-              src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
-              class="movie__poster--img"
-              alt=""
-            ></img>
-          </div>
-          <div class="movie__info--container">
-            <div class="movie__title">The Fast and the Furious</div>
-            <div class="movie__year">2001</div>
-          </div>
-        </div>
-      </div>
-      <div class="movie__container">
-        <div class="movie">
-          <div class="movie__poster">
-            <img
-              src="https://m.media-amazon.com/images/M/MV5BMTUxNTk5MTE0OF5BMl5BanBnXkFtZTcwMjA2NzY3NA@@._V1_SX300.jpg"
-              class="movie__poster--img"
-              alt=""
-            ></img>
-          </div>
-          <div class="movie__info--container">
-            <div class="movie__title">Fast Five</div>
-            <div class="movie__year">2011</div>
-          </div>
-        </div>
-      </div>
-      <div class="movie__container">
-        <div class="movie">
-          <div class="movie__poster">
-            <img
-              src="https://m.media-amazon.com/images/M/MV5BYjQ1ZTMxNzgtZDcxOC00NWY5LTk3ZjAtYzRhMDhlNDZlOWEzXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-              class="movie__poster--img"
-              alt=""
-            ></img>
-          </div>
-          <div class="movie__info--container">
-            <div class="movie__title">Fast & Furious</div>
-            <div class="movie__year">2009</div>
-          </div>
-        </div>
-      </div>
-      <div class="movie__container">
-        <div class="movie">
-          <div class="movie__poster">
-            <img
-              src="https://m.media-amazon.com/images/M/MV5BMzExYjcyYWMtY2JkOC00NDUwLTg2OTgtMDI3MGY2OWQzMDE2XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-              class="movie__poster--img"
-              alt=""
-            ></img>
-          </div>
-          <div class="movie__info--container">
-            <div class="movie__title">2 Fast 2 Furious</div>
-            <div class="movie__year">2003</div>
-          </div>
-        </div>
-      </div>
-      <div class="movie__container">
-        <div class="movie">
-          <div class="movie__poster">
-            <img
-              src="https://m.media-amazon.com/images/M/MV5BMTQ2NTMxODEyNV5BMl5BanBnXkFtZTcwMDgxMjA0MQ@@._V1_SX300.jpg"
-              class="movie__poster--img"
-              alt=""
-            ></img>
-          </div>
-          <div class="movie__info--container">
-            <div class="movie__title">
-              The Fast and the Furious: Tokyo Drift
-            </div>
-            <div class="movie__year">2006</div>
+          <div className="movie__info--container">
+            <div className="movie__title">{movie.Title}</div>
+            <div className="movie__year">{movie.Year}</div>
           </div>
         </div>
       </div>
