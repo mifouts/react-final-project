0fb275ad913d8a75fd22884e72160415a21f21d2
mifouts <fouts.mitch15@gmail.com>
2023-02-20 14:09:08 -0500

    using ternary operators to give a rating or plot

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -19,6 +19,8 @@ function Landing() {
     setLoading(false);
   };
 
+  console.log(selectedMovie);
+
   return (
     <div>
       <div className="container landing__container">
@@ -67,20 +69,28 @@ function Landing() {
                 </div>
               </div>
             ))}
-            {selectedMovie && (
-              <div className="selected__movie--container">
-                <div className="selected__movie--poster">
-                  <img
-                    src={selectedMovie.Poster}
-                    alt=""
-                    className="selected__movie--poster-img"
-                  />
-                </div>
-                <h2 className="selectedMovie__title">{selectedMovie.Title}</h2>
-                <h3 className="selectedMovie__year">{selectedMovie.Year}</h3>
-                <h4 className="selectedMovie__rating">{selectedMovie.Rated}</h4>
-                <p className="selectedMovie__plot">{selectedMovie.Plot}</p>
-              </div>
+          </div>
+        )}
+        {selectedMovie && (
+          <div className="selected__movie--container">
+            <div className="selected__movie--poster">
+              <img
+                src={selectedMovie.Poster}
+                alt=""
+                className="selected__movie--poster-img"
+              />
+            </div>
+            <h2 className="selectedMovie__title">{selectedMovie.Title}</h2>
+            <h3 className="selectedMovie__year">{selectedMovie.Year}</h3>
+            {selectedMovie.Rated ? (
+              <h4 className="selectedMovie__rating">{selectedMovie.Rated}</h4>
+            ) : (
+              <p className="selectedMovie__unavailable">Rating not available</p>
+            )}
+            {selectedMovie.Plot ? (
+              <p className="selectedMovie__plot">{selectedMovie.Plot}</p>
+            ) : (
+              <p className="selectedMovie__unavailable">Plot not available</p>
             )}
           </div>
         )}
